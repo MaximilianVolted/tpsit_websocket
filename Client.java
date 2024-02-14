3c127f9904669af3983e3ff8fdb324ceec98ed33
Maximilian Volt. <massimilianovolturno+git@gmail.com>
2024-02-14 11:39:40 +0100

    Updated Client.java

--- Client.java
+++ Client.java
@@ -1,4 +1,25 @@
+import java.io.*;
+import java.net.*;
+
 public class Client
 {
+  public static void main(String[] args)
+  {
+    int port = 12345;
+    String hostname = "localhost";
 
+    try
+    (
+      Socket socket = new Socket(hostname, port);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    )
+    {
+      String data = in.readLine();
+      System.out.println((data != null) ? data : "[No data found]");
+    }
+    catch (IOException exception)
+    {
+      System.err.println("Error: " + exception.getMessage());
+    }
+  }
 }
